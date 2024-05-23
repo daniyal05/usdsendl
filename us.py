59d6a829f9f6917701d7006229d18fe95b230a1e
Daniyal <101132839+daniyal05@users.noreply.github.com>
2024-05-23 18:17:18 +0300

    Update us.py

--- us.py
+++ us.py
@@ -4,7 +4,7 @@ import time
 
 url = "https://www.google.com/search?q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&sca_esv=80879160ea65325f&sca_upv=1&sxsrf=ADLYWIJ-_LQ5cIDjpY2NQR82tUK4i5z9Zg%3A1716368054780&ei=trJNZvKhL4zCwPAP9Y2JwAQ&udm=&oq=dolar&gs_lp=Egxnd3Mtd2l6LXNlcnAiBWRvbGFyKgIIADIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzINEAAYgAQYsAMYQxiKBTINEAAYgAQYsAMYQxiKBUiGDFAAWABwAXgBkAEAmAEAoAEAqgEAuAEByAEAmAIBoAISmAMAiAYBkAYKkgcBMaAHAA&sclient=gws-wiz-serp"
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
+    'User-Agent': 'Your user agent'}
 
 
 page = requests.get(url, headers=headers)
